bbdc2c9b2ad53e5f80e233301046c30b3f3b7c09
MAKAREMBO <dan5050441@yandex.ru>
2021-09-17 11:25:49 +0300

    Laba1

--- src/ru/mirea/test/Main.java
+++ src/ru/mirea/test/Main.java
@@ -3,9 +3,27 @@ package ru.mirea.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hi");
-        System.out.println("Tester");
-        System.out.println("Third");
+        int[] myArr = {1, 2, 3, 4};
+        int sum = 0;
+        int x = 0;
+        do {
+            sum += myArr[x];
+            x++;
+        }
+        while (x < myArr.length);
+        System.out.println(sum);
+
+
+        while (x < myArr.length) {
+            sum += myArr[x];
+            x++;
+        }
+        System.out.println(sum);
+
+        sum = 0;
+        for (x = 0; x < myArr.length; x++) {
+            sum += myArr[x];
+        }
+        System.out.println(sum);
     }
 }
