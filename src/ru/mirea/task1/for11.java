606ad70e7f0749d8e9c42a2e3512fd023fa9faf2
MAKAREMBO <dan5050441@yandex.ru>
2021-09-17 11:24:40 +0300

    Laba1

---
+++ src/ru/mirea/task1/for11.java
@@ -0,0 +1,30 @@
+package ru.mirea.task1;
+
+
+public class for11{
+
+    public static void main(String[] args) {
+        int[] myArr = {1, 2, 3, 4};
+        int sum = 0;
+        int x = 0;
+        do {
+            sum += myArr[x];
+            x++;
+        }
+        while (x < myArr.length);
+        System.out.println(sum);
+
+
+        while (x < myArr.length) {
+            sum += myArr[x];
+            x++;
+        }
+        System.out.println(sum);
+
+        sum = 0;
+        for (x = 0; x < myArr.length; x++) {
+            sum += myArr[x];
+        }
+        System.out.println(sum);
+    }
+}
