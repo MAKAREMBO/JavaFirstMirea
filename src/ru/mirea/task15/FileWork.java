c0b1ada5c3ccda15b4355606131bb90eff4dc211
MAKAREMBO <dan5050441@yandex.ru>
2021-12-12 03:38:41 +0300

    task11-15

---
+++ src/ru/mirea/task15/FileWork.java
@@ -0,0 +1,20 @@
+package ru.mirea.task15.opt1;
+import java.io.*;
+import java.util.Scanner;
+
+public class FileWork {
+    public static void main(String[] args) throws IOException
+    {
+        File file = new File("C:\\Users\\file.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+
+        fileOutputStream.write(input.getBytes());
+
+        fileOutputStream.close();
+    }
+
+}
